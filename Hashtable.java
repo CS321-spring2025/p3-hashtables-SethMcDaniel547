1622fc7f4a6eabd6fda9f1ecf593cf46e4d4b3b9
SethMcDaniel547 <sethmcdaniel547@u.boisestate.edu>
2025-03-12 19:00:41 -0600

    HashTable started

---
+++ Hashtable.java
@@ -0,0 +1,17 @@
+public abstract class Hashtable {
+    HashObject[] table;
+    public HashTable (int size) {
+        this.table = new HashObject[size];
+    }
+    public void insert(HashObject newItem) {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public HashObject search() {
+
+    }
+}
