c78213668d0436ce51c9e5fe272d5b40bfc808fa
SethMcDaniel547 <sethmcdaniel547@u.boisestate.edu>
2025-03-12 18:24:47 -0600

    TwinPrimeGen started

---
+++ TwinPrimeGenerator.java
@@ -0,0 +1,27 @@
+//import java.util.LinkedList;
+
+public class TwinPrimeGenerator {
+    public static int generateTwinPrime(int min, int max) {
+        //LinkedList<Integer> primes = new LinkedList<Integer>();
+        for (int num = min; num <= max - 2; num++) {
+            if (isPrime(num) && isPrime(num + 2)) {
+                return num + 2;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
